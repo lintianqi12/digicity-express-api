a7e078bd57f7ea1da6e85fa44fd7350f39713041
lintianqi12 <1498912967@qq.com>
2016-10-07 16:45:29 +0800

    post

--- demo-mongoose-save-data/index.js
+++ demo-mongoose-save-data/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
-var mongoose = require('mongoose');
 
+var mongoose = require('mongoose');
 
 //链接数据库和服务器端
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/express-api');
+mongoose.connect('mongodb://localhost:27017/digicity-express-api');
 
+//验证是否连接成功
 var db = mongoose.connection;
 db.on('error', console.log);
 db.once('open', function() {
@@ -14,19 +15,30 @@ db.once('open', function() {
 });
 
 
-app.post('/posts', function(req, res) {
-  var mpost = {
-    title: "lin",
-    content: "tianqi"
-  };
-  mpost.save(function(err){
+var Schema = mongoose.Schema;
+
+//记录要存储的数据的基本结构
+var PostSchema = new Schema(
+  {
+    title: String,
+    content: String
+  }
+);
+
+var Post = mongoose.model('Post',PostSchema)
+
+
+//路由代码
+app.post('/posts', function(req, res){
+  console.log('run post.save()');
+  //模块 实例化
+  var post = new Post({title:"myTitle", content: "myConent"})
+  post.save(function(err){
     if(err) return console.log(err);
-    console.log('saved!');
-  })
-  res.json({message:'保存成功！'})
-})
+    console.log('saved');
+  });
+});
 
-
-app.listen(3000, function() {
-  console.log('running on port 3000')
-})
+app.listen(3000, function(){
+  console.log('running on port 3000.....');
+});
